e6aa12557626c99b943fd635e7487763198b767d
dmitrii-pov <popkovd.o@yandex.ru>
2026-03-05 17:03:34 +0300

    feat(core): add reflection-free container

---
+++ src/Container/ContainerException.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LPhenom\Core\Container;
+
+use LPhenom\Core\Exception\LPhenomException;
+
+final class ContainerException extends LPhenomException
+{
+}
+
